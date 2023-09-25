ef97586d937e6f481e87548572322bd60be518e3
xiaoliang <651147191@qq.com>
2023-09-25 14:22:53 +0800

    线程两种创建方式

---
+++ src/main/java/chapterone/CreateThread.java
@@ -0,0 +1,23 @@
+package chapterone;
+
+public class CreateThread {
+    public static void main(String[] args) {
+        // 方式1
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                System.out.println("无语了");
+            }
+        };
+        thread.start();
+
+        // 方式二
+        Runnable run1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("方式二");
+            }
+        };
+        new Thread(run1,"run2").start();
+    }
+}
